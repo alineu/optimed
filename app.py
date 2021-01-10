2da12d8aa1a41a5d0d6ad2330f04e125ad0906da
Ali <alimehdirahim@gmail.com>
2021-01-09 23:51:33 -0500

    testing the radar chart

--- app.py
+++ app.py
@@ -14,8 +14,7 @@ from bs4 import BeautifulSoup, SoupStrainer
 import difflib
 from sqlalchemy import desc
 from stop_words import stops as stop_words
-# from nltk.corpus import stopwords
-#stop_words = stopwords.words('english')
+
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -24,10 +23,6 @@ from models import OptiMedResult
 db.create_all()
 Base = db.Model.metadata.reflect(db.engine)
 conditions_table = db.Model.metadata.tables['drugs']
-# pharmacies_latlon = db.Model.metadata.tables['pharmacies_latlon']
-# pharmacies_detail = db.Model.metadata.tables['pharmacies_detail']
-# from utils.app_utils import *
-
 q = Queue(connection=conn)
 
 
@@ -101,25 +96,7 @@ def get_winner_drugs(user_condition, num_retries=10, zipcode='02115'):
         except IndexError as e:
             pass
     return results_dict
-    # print(results_dict)
-    # r = list(map(lambda x: json.dumps(x), results_dict))
-    # # results_dict = json.loads(r)
-    # print(results_dict)
-    # json_object = json.dumps(results_dict, indent = 4) 
-    # return json_object
 
-
-# conn = db.session.connection()
-# curs = conn.connection.cursor()
-# curs.execute("ROLLBACK")
-# conn.commit()
-
-        # return drug_candidates
-        # except IndexError as error:
-        #     if attempt_no < (num_retries - 1):
-        #         pass
-        #     else:
-        #         raise error
 
 
 def find_meds(user_condition):
@@ -132,7 +109,6 @@ def find_meds(user_condition):
         result = OptiMedResult(
             url=user_condition,
             all_drugs=all_drugs
-            # ,all_prices=all_prices
         )
         print('adding')
         db.session.add(result)
@@ -145,56 +121,12 @@ def find_meds(user_condition):
         errors.append("Unable to add the item to database.")
         return {"error": errors}
 
-# def count_and_save_words(url):
-
-#     errors = []
-
-#     try:
-#         r = requests.get(url)
-#     except:
-#         errors.append(
-#             "Unable to get URL. Please make sure it's valid and try again."
-#         )
-#         return {"error": errors}
-
-#     # text processing
-#     raw = BeautifulSoup(r.text, 'html.parser').get_text()
-#     nltk.data.path.append('./nltk_data/')  # set the path
-#     tokens = nltk.word_tokenize(raw)
-#     text = nltk.Text(tokens)
-
-#     # remove punctuation, count raw words
-#     nonPunct = re.compile('.*[A-Za-z].*')
-#     raw_words = [w for w in text if nonPunct.match(w)]
-#     raw_word_count = Counter(raw_words)
-
-#     # stop words
-#     no_stop_words = [w for w in raw_words if w.lower() not in stops]
-#     no_stop_words_count = Counter(no_stop_words)
-
-#     # save the results
-#     try:
-#         result = Result(
-#             url=url,
-#             result_all=raw_word_count,
-#             result_no_stop_words=no_stop_words_count
-#         )
-#         db.session.add(result)
-#         db.session.commit()
-#         return result.id
-#     except:
-#         errors.append("Unable to add item to database.")
-#         return {"error": errors}
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
-    #return render_template('main.html')
 
 @app.route('/about', methods=['GET', 'POST'])
 def about():
-    # return render_template('index.html')
     return render_template('about.html')
 
 @app.route('/start', methods=['POST'])
@@ -203,13 +135,14 @@ def get_counts():
     data = json.loads(request.data.decode())
     print(data)
     url = data["url"]
+
     # start job
     job = q.enqueue_call(
         func=find_meds, args=(url,), result_ttl=5000
     )
+
     # return created job id
     print(url)
-    # print('abolfazal')
 
     print(job.get_id())
     return job.get_id()
@@ -220,18 +153,6 @@ def printMsg():
     app.logger.error('testing error log')
     app.logger.info('testing info log')
     return "Check your console"
-# @app.route('/findmed', methods=['POST'])
-# def find_meds():
-#     # get url
-#     data = json.loads(request.data.decode())
-#     user_condition = data["user_condition"]
-    
-#     # start job
-#     job = q.enqueue_call(
-#         func=get_winner_drugs, args=(user_condition,), result_ttl=2500
-#     )
-#     # return created job id
-#     return job.get_id()
 
 
 @app.route("/results/<job_key>", methods=['GET'])
@@ -240,16 +161,9 @@ def get_results(job_key):
     job = Job.fetch(job_key, connection=conn)
 
     if job.is_finished:
-        # print('miaumiau')
 
         result = OptiMedResult.query.filter_by(id=job.result).first()
         print(result)
-        # results = sorted(
-        #     result.all_drugs.items(),
-        #     key=operator.itemgetter(1),
-        #     reverse=True
-        # )[:3]
-        # results = sorted(result.all_drugs.items())
         results = result.all_drugs
         print(results)
         return jsonify(results)
--- models.py
+++ models.py
@@ -17,21 +17,6 @@ class OptiMedResult(db.Model):
     def __repr__(self):
         return '<id {}>'.format(self.id)
 
-class Result(db.Model):
-    __tablename__ = 'results'
-    __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String())
-    result_all = db.Column(JSON)
-    result_no_stop_words = db.Column(JSON)
-
-    def __init__(self, url, result_all, result_no_stop_words):
-        self.url = url
-        self.result_all = result_all
-        self.result_no_stop_words = result_no_stop_words
-
-    def __repr__(self):
-        return '<id {}>'.format(self.id)
 
 
 # class DrugSearchResult(db.Model):
